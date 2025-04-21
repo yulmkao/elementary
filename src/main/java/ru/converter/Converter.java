1b871abf4c6213771ae3c144161bc3e0f79a058a
yulmkao <yulmkao@gmail.com>
2025-04-21 11:16:51 +0300

    1.9 Конвертер валюты

---
+++ src/main/java/ru/converter/Converter.java
@@ -0,0 +1,20 @@
+package ru.converter;
+
+public class Converter {
+    public static float rubleToEuro(float value) {
+        float rsl = value / 100;
+        return rsl;
+    }
+
+    public static float rubleToDollar(float value) {
+        float rsl = value / 90;
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        float euro = Converter.rubleToEuro(140);
+        float dollar = Converter.rubleToDollar(300);
+        System.out.println("140 rubles are " + euro + " euros.");
+        System.out.println("300 rubles are " + dollar + " dollars.");
+    }
+}
