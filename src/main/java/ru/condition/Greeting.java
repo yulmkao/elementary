119e913f84cd665d5b8a45e0866a41b899fc913d
yulmkao <yulmkao@gmail.com>
2025-04-21 14:33:03 +0300

    1.14 String

---
+++ src/main/java/ru/condition/Greeting.java
@@ -0,0 +1,12 @@
+package ru.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        System.out.println(idea);
+        idea += " But I am a newbie. ";
+        int year = 2025;
+        idea += year;
+        System.out.println(idea);
+    }
+}
