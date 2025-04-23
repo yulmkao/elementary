b3c448af2080ea2d87df89129f9e32a3c6922d0c
yulmkao <yulmkao@gmail.com>
2025-04-23 12:30:08 +0300

    2.2 Что такое import

---
+++ src/main/java/ru/math/MathFunction.java
@@ -0,0 +1,20 @@
+package ru.math;
+
+public class MathFunction {
+
+    public static double multiply(double first, double second) {
+        return first * second;
+    }
+
+    public static double sum(double first, double second) {
+        return first + second;
+    }
+
+    public static double subtract(double first, double second) {
+        return first - second;
+    }
+
+    public static double divide(double first, double second) {
+        return first / second;
+    }
+}
