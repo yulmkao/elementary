f740c12c3f14e38b029cf13b7d7f67db0b03f2a9
yulmkao <yulmkao@gmail.com>
2025-04-24 16:46:44 +0300

    4.1 Цикл for. Определение

---
+++ src/main/java/ru/loop/LoopFor.java
@@ -0,0 +1,9 @@
+package ru.loop;
+
+public class LoopFor {
+    public static void main(String[] args) {
+        for (int i = 5; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
