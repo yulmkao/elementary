cf1b046e6b427d0b60c4251b100fc8e997b4dfd3
yulmkao <yulmkao@gmail.com>
2025-04-25 23:11:03 +0300

    4.14 Генерация конструкций в IDEA

---
+++ src/main/java/ru/live/Main.java
@@ -0,0 +1,9 @@
+package ru.live;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
