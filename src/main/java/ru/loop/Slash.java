aad9ef6d2a8cf5def5b62f1895de6d3b62082cc4
yulmkao <yulmkao@gmail.com>
2025-04-25 23:25:04 +0300

    4.13 Крест в псевдографике

--- src/main/java/ru/loop/Slash.java
+++ src/main/java/ru/loop/Slash.java
@@ -4,11 +4,9 @@ public class Slash {
     public static void draw(int size) {
         for (int row = 0; row < size; row++) {
             for (int cell = 0; cell < size; cell++) {
-                boolean left = row == cell;
-                boolean right = row + cell == size - 1;
-                if (left) {
+                if (row == cell) {
                     System.out.print("0");
-                } else if (right) {
+                } else if (row + cell == size - 1) {
                     System.out.print("0");
                 } else {
                     System.out.print(" ");
