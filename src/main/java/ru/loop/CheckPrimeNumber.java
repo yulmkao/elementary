06321b32655703be6f3043ae95927bd8651e1c52
yulmkao <yulmkao@gmail.com>
2025-04-25 13:27:41 +0300

    4.8 Простое число. Оператор прерывания break

---
+++ src/main/java/ru/loop/CheckPrimeNumber.java
@@ -0,0 +1,18 @@
+package ru.loop;
+
+public class CheckPrimeNumber {
+    public static boolean check(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        boolean prime = true;
+        for (int index = 2; index < number; index++) {
+            if (number % index == 0) {
+                prime = false;
+                break;
+            }
+        }
+        return  prime;
+    }
+}
+
