807f9b84338912f3d7e1fcb499445a1c897420d1
yulmkao <yulmkao@gmail.com>
2025-04-26 20:45:46 +0300

    5.13 Массив заполнен true или false

---
+++ src/main/java/ru/array/Check.java
@@ -0,0 +1,13 @@
+package ru.array;
+
+public class Check {
+    public static boolean mono(boolean[] data) {
+        boolean reference = data[0];
+        for (int index = 0; index < data.length; index++) {
+            if (data[index] != reference) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
