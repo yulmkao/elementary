76776e9df82bc2fcc95532c6758e5160c8105992
yulmkao <yulmkao@gmail.com>
2025-04-26 20:15:03 +0300

    5.12 Перевернуть массив

---
+++ src/test/java/ru/array/TurnTest.java
@@ -0,0 +1,24 @@
+package ru.array;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class TurnTest {
+
+    @Test
+    void whenTurnArrayWithEvenAmountOfElementsThenTurnedArray() {
+        int[] input = new int[] {4, 1, 6, 2};
+        int[] result = Turn.back(input);
+        int[] expected = new int[] {2, 6, 1, 4};
+        assertThat(result).containsExactly(expected);
+    }
+
+    @Test
+    void whenTurnArrayWithOddAmountOfElementsThenTurnedArray() {
+        int[] input = new int[] {1, 2, 3, 4, 5};
+        int[] result = Turn.back(input);
+        int[] expected = new int[] {5, 4, 3, 2, 1};
+        assertThat(result).containsExactly(expected);
+    }
+}
