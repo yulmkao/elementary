d88f94a99cd7cb1fde27d9231a7dbba9c0f956d4
yulmkao <yulmkao@gmail.com>
2025-04-27 19:18:43 +0300

    5.17 Слово заканчивается на...

---
+++ src/main/java/ru/array/EndsWith.java
@@ -0,0 +1,12 @@
+package ru.array;
+
+public class EndsWith {
+    public static boolean endsWith(char[] word, char[] post) {
+        for (int index = 0; index < post.length; index++) {
+            if (word[word.length - 1 - index] != post[post.length - 1 - index]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
