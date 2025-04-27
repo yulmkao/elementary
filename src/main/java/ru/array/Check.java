ad5dc6cc9346b3897f3784ef61ef2df37df3f63c
yulmkao <yulmkao@gmail.com>
2025-04-27 14:35:51 +0300

    5.13 Массив заполнен true или false

--- src/main/java/ru/array/Check.java
+++ src/main/java/ru/array/Check.java
@@ -3,7 +3,7 @@ package ru.array;
 public class Check {
     public static boolean mono(boolean[] data) {
         boolean reference = data[0];
-        for (int index = 0; index < data.length; index++) {
+        for (int index = 1; index < data.length; index++) {
             if (data[index] != reference) {
                 return false;
             }
