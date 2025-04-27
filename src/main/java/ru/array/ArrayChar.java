9a96911086c60ed03c071aec119092f8c07112a9
yulmkao <yulmkao@gmail.com>
2025-04-27 14:51:21 +0300

    5.14 Слово начинается с...

---
+++ src/main/java/ru/array/ArrayChar.java
@@ -0,0 +1,13 @@
+package ru.array;
+
+public class ArrayChar {
+
+    public static boolean startsWith(char[] word, char[] pref) {
+        for (int index = 0; index < pref.length; index++) {
+            if (pref[index] != word[index]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
