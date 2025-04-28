daa57322c1a58aee6dbb40d64cb6a05d60f9de88
yulmkao <yulmkao@gmail.com>
2025-04-29 00:32:31 +0300

    5.28 Моно столбец в матрице

--- src/main/java/ru/array/MatrixCheck.java
+++ src/main/java/ru/array/MatrixCheck.java
@@ -2,11 +2,20 @@ package ru.array;
 
 public class MatrixCheck {
     public static boolean monoHorizontal(char[][] board, int row) {
-        for (int col = 0; col < board[row].length; col++) {
-                if (board[row][col] != 'X') {
+        for (int column = 0; column < board[row].length; column++) {
+                if (board[row][column] != 'X') {
                     return false;
                 }
             }
+        return true;
+    }
+
+    public static boolean monoVertical(char[][] board, int column) {
+        for (int row = 0; row < board.length; row++) {
+            if (board[row][column] != 'X') {
+                return false;
+            }
+        }
         return true;
     }
 }
