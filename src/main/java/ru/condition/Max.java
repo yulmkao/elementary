a4b434d372cd427ca43f2b24466c8d5c51cb428a
yulmkao <yulmkao@gmail.com>
2025-05-06 17:41:41 +0300

    7.15 Перегрузить метод max для трех чисел

--- src/main/java/ru/condition/Max.java
+++ src/main/java/ru/condition/Max.java
@@ -5,6 +5,14 @@ public class Max {
         return left > right ? left : right;
     }
 
+    public static int max(int left, int middle, int right) {
+        return max(max(left, middle), right);
+    }
+
+    public static int max(int left, int middle, int middle2,  int right) {
+        return max(max(left, middle, middle2), right);
+    }
+
     public static void main(String[] args) {
         int left = 11;
         int right = 5;
--- src/test/java/ru/condition/MaxTest.java
+++ src/test/java/ru/condition/MaxTest.java
@@ -41,4 +41,46 @@ class MaxTest {
         int expected = 5;
         assertThat(result).isEqualTo(expected);
     }
+
+    @Test
+    void whenMax1To25To20Then5() {
+        int left = 1;
+        int middle = 25;
+        int right = 20;
+        int result = Max.max(left, middle, right);
+        int expected = 25;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenMax99To239To519Then519() {
+        int left = 99;
+        int middle = 239;
+        int right = 519;
+        int result = Max.max(left, middle, right);
+        int expected = 519;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenMax44To247To22To35Then247() {
+        int left = 44;
+        int middle = 246;
+        int middle2 = 22;
+        int right = 35;
+        int result = Max.max(left, middle, middle2, right);
+        int expected = 246;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenMax6To42To505To1Then505() {
+        int left = 6;
+        int middle = 42;
+        int middle2 = 505;
+        int right = 1;
+        int result = Max.max(left, middle, middle2, right);
+        int expected = 505;
+        assertThat(result).isEqualTo(expected);
+    }
 }
